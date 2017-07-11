ca4387c516ae5962025955f6bb1d840c5de7805b
llgoncalves <luanlg.cco@gmail.com>
2017-07-11 18:43:39 -0300

    resolução do sistema

--- min_quadrados.py
+++ min_quadrados.py
@@ -1,32 +1,42 @@
+import numpy as np
+
 dados = []
 grau_polinomio = 1
 
-# A*X = B 
-A = [[0]*(grau_polinomio + 1)]*(grau_polinomio + 1)
-X = [0]*grau_polinomio
+# A*X = B
+A = []
+for i in range(grau_polinomio + 1):
+	A.append([0]*(grau_polinomio+1))
+
+X = [0]*(grau_polinomio+1)
 B = [[]]*(grau_polinomio + 1)
+A = [[0,0],[0,0]]
 
 with open("dados_1", "r") as f:
 	for line in f:
 		line = line.split('\t')
 		dados.append([float(line[0]), float(line[1])])
-	
+
 for i in range(grau_polinomio + 1):
 	for j in range(i, grau_polinomio + 1):
 		x = 0
+
 		for k in range(len(dados)):
-			x += dados[k][0]**(grau_polinomio + 1 - j -i)
-		
+			x += dados[k][0]**(grau_polinomio+1 - j -i)
 		A[i][j] = x
 		A[j][i] = x
 
 for i in range(grau_polinomio+1):
-	soma  = 0 
+	soma  = 0
 	for j in dados:
 		soma += j[1]*j[0]**i
 	B[grau_polinomio - i] = soma
 
-print "Dados de entrada:\n",dados
-print "\nA:\n", A 
-print "\nB:\n", B	
 
+X = np.linalg.solve(np.array(A), np.array(B))
+
+print "Determinante A ",np.linalg.det(A)
+print "Dados de entrada:\n",dados
+print "\nA:\n", A
+print "\nB:\n", B
+print "\nX:\n", X
